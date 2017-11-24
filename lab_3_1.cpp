37d3f34cd2485291cd2d81237df7462cadf19635
Unknown <nipapornnak@cvc-cha.ac.th>
2017-11-24 13:33:21 +0700

    ข้อที่1

--- lab_3_1.cpp
+++ lab_3_1.cpp
@@ -4,5 +4,13 @@ using namespace std;
 
 int main() {
   // เขียนโปรแกรมเพื่อแสดงผลตามที่กำหนด
+  cout << "ms." << endl;
+  cout << "nipaporn." << endl;
+  cout << "nakcharoen." << endl;
+  cout << "am." << endl;
+  cout << "female." << endl;
+  cout << "09/10/2541." << endl;
+  cout << "listen to music." << endl;
+  cout << "single." << endl;
   return 0;
 }
